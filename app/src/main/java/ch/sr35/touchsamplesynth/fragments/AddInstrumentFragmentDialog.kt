c05de2cd736deb88fbb86bd1a04532901588348d
Stonerose35 <fuerholz@gmx.ch>
2023-09-17 00:29:32 +0200

    added "add and "delete" buttons, pt2

---
+++ app/src/main/java/ch/sr35/touchsamplesynth/fragments/AddInstrumentFragmentDialog.kt
@@ -0,0 +1,82 @@
+package ch.sr35.touchsamplesynth.fragments
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.Button
+import android.widget.ListView
+import android.widget.Spinner
+import androidx.fragment.app.DialogFragment
+import ch.sr35.touchsamplesynth.R
+import ch.sr35.touchsamplesynth.TouchSampleSynthMain
+import ch.sr35.touchsamplesynth.audio.MusicalSoundGenerator
+import ch.sr35.touchsamplesynth.audio.instruments.SimpleSubtractiveSynthK
+import ch.sr35.touchsamplesynth.audio.instruments.SineMonoSynthK
+import ch.sr35.touchsamplesynth.views.TouchElement
+import dalvik.system.PathClassLoader
+
+/**
+ * A simple [Fragment] subclass.
+ * create an instance of this fragment.
+ */
+class AddInstrumentFragmentDialog(private val generatorsList: ListView) : DialogFragment() {
+
+    private var soundGenerators = ArrayList<MusicalSoundGenerator>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_instrument_dialog, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (soundGenerators.isEmpty()) {
+            this.context?.let {
+                soundGenerators.add(SineMonoSynthK(it))
+                soundGenerators.add(SimpleSubtractiveSynthK(it))
+            }
+        }
+
+        val spinnerSoundGenerator=view.findViewById<Spinner>(R.id.spinnerSoundgenerator)
+        val spinnerSoundGeneratorAdapter =
+            this.context?.let { SoundGeneratorSpinnerAdapter(soundGenerators, it) }
+        spinnerSoundGenerator.adapter = spinnerSoundGeneratorAdapter
+        spinnerSoundGenerator.setSelection(0)
+        spinnerSoundGenerator.onItemSelectedListener = spinnerSoundGeneratorAdapter
+        val buttonOk=view.findViewById<Button>(R.id.add_instr_button_ok)
+        buttonOk.setOnClickListener {
+            if (spinnerSoundGeneratorAdapter?.soundGenerator != null) {
+                context?.let { it1 ->
+                    (context as TouchSampleSynthMain).soundGenerators.add(
+                    spinnerSoundGeneratorAdapter.soundGenerator!!.generateAttachedInstance(
+                        it1
+                    ))
+                    generatorsList.invalidateViews()
+                }
+            }
+
+            this.dismiss()
+        }
+        val buttonCancel = view.findViewById<Button>(R.id.add_instr_button_cancel)
+        buttonCancel.setOnClickListener {
+            this.dismiss()
+        }
+    }
+
+    companion object {
+
+    }
+}
