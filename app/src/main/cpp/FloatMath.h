ea14e0de1f98b2192f965617fce2136b426f974d
Stonerose35 <fuerholz@gmx.ch>
2023-11-04 17:09:51 +0100

    added float math, completed param initialization

---
+++ app/src/main/cpp/FloatMath.h
@@ -0,0 +1,12 @@
+//
+// Created by philipp on 04.11.23.
+//
+
+#ifndef TOUCHSAMPLESYNTH_FLOATMATH_H
+#define TOUCHSAMPLESYNTH_FLOATMATH_H
+#define F f
+#define COMBINE(A,B) A ## B
+#define EVAL(A,B) COMBINE(A,B)
+#define M_PI_F EVAL(M_PI,F)
+
+#endif //TOUCHSAMPLESYNTH_FLOATMATH_H
--- app/src/main/cpp/SineOscillator.cpp
+++ app/src/main/cpp/SineOscillator.cpp
@@ -4,33 +4,33 @@
 
 #include "SineOscillator.h"
 #include <cmath>
-
+#include "FloatMath.h"
 
 float SineOscillator::getNextSample() {
     currentPhase += phaseIncrement;
-    if (currentPhase > 2.0f*M_PI)
+    if (currentPhase > 2.0f*M_PI_F)
     {
-        currentPhase -= 2.0f*M_PI;
+        currentPhase -= 2.0f*M_PI_F;
     }
     return sinf(currentPhase);
 }
 
 void SineOscillator::setNote(float n) {
-    float freq = powf(2,n/12.0f)*440.0f;
-    phaseIncrement = freq/samplingRate*2.0f*M_PI;
+    float freq = powf(2.0f,n/12.0f)*440.0f;
+    phaseIncrement = freq/samplingRate*2.0f*M_PI_F;
 }
 
 SineOscillator::SineOscillator(float sr)
 {
     samplingRate = sr;
     currentPhase = 0.0f;
-    phaseIncrement=0.0f;
+    phaseIncrement = 432.0f/samplingRate*M_PI_F*2.0f;
 }
 
 SineOscillator::SineOscillator() {
     samplingRate = 48000.0f;
-    currentPhase = 0;
-    setNote(0.0f);
+    currentPhase = 0.0f;
+    phaseIncrement = 432.0f/samplingRate*M_PI_F*2.0f;
 }
 
 
