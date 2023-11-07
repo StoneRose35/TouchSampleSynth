5e3cf006e614f26fd5bad855e92a8087158c36b4
Stonerose35 <fuerholz@gmx.ch>
2023-11-07 23:21:19 +0100

    working on midi implementation (compileable)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "ch.sr35.touchsamplesynth"
-        minSdk = 26
+        minSdk = 29
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
