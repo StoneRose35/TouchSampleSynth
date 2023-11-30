3d24abcfddad75bde8a54c651eecf2f1ab61cab8
Stonerose35 <fuerholz@gmx.ch>
2023-11-30 22:34:18 +0100

    changed target sdk to 33

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
     defaultConfig {
         applicationId = "ch.sr35.touchsamplesynth"
         minSdk = 29
-        targetSdk = 31
-        versionCode = 1
+        targetSdk = 33
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
