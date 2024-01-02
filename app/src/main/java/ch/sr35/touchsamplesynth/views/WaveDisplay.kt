8ce1dceb0b05888d650a32c7410d260bd46b3212
Stonerose35 <fuerholz@gmx.ch>
2024-01-02 23:19:01 +0100

    wave display: layout and useability improvements

--- app/src/main/java/ch/sr35/touchsamplesynth/views/WaveDisplay.kt
+++ app/src/main/java/ch/sr35/touchsamplesynth/views/WaveDisplay.kt
@@ -10,14 +10,18 @@ import android.view.MotionEvent
 import android.view.View
 import ch.sr35.touchsamplesynth.R
 import ch.sr35.touchsamplesynth.graphics.Converter
-import kotlin.math.abs
 
 val MARKER_SIZE= Converter.toPx(12)
 val MARKER_LINE_WIDTH=Converter.toPx(3)
+val LOOP_SIGN_RADIUS=Converter.toPx(8)
+val LOOP_SIGN_MARGIN=Converter.toPx(3)
+val LOOP_SIGN_STROKE_WIDTH=Converter.toPx(3).toFloat()
 
 class WaveDisplay(context: Context, attributes: AttributeSet): View(context,attributes)   {
     private val waveColor:Paint = Paint()
-    private val markersColor:Paint = Paint()
+    private val sampleMarkersColor:Paint = Paint()
+    private val loopMarkersColor:Paint = Paint()
+    private val loopSignColor:Paint = Paint()
     private val backgroundColor:Paint = Paint()
     private var sampleData=ArrayList<Float>()
     var waveViewBuffer: Bitmap?=null
@@ -41,9 +45,18 @@ class WaveDisplay(context: Context, attributes: AttributeSet): View(context,attr
         waveColor.style = Paint.Style.STROKE
         waveColor.isAntiAlias = false
 
-        markersColor.color = context.getColor(R.color.black)
-        markersColor.style = Paint.Style.FILL
-        markersColor.isAntiAlias = true
+        sampleMarkersColor.color = context.getColor(R.color.dark_green)
+        sampleMarkersColor.style = Paint.Style.FILL
+        sampleMarkersColor.isAntiAlias = true
+
+        loopMarkersColor.color = context.getColor(R.color.medium_green)
+        loopMarkersColor.style = Paint.Style.FILL
+        loopMarkersColor.isAntiAlias = true
+
+        loopSignColor.color = context.getColor(R.color.medium_green)
+        loopSignColor.style = Paint.Style.STROKE
+        loopSignColor.strokeWidth= LOOP_SIGN_STROKE_WIDTH
+        loopSignColor.isAntiAlias = true
 
         backgroundColor.color = context.getColor(R.color.white)
         backgroundColor.style = Paint.Style.FILL
@@ -57,31 +70,60 @@ class WaveDisplay(context: Context, attributes: AttributeSet): View(context,attr
 
         canvas.drawRect(0.0f,0.0f,width.toFloat(),height.toFloat(),backgroundColor)
         waveViewbufferRect.top=0
-        waveViewbufferRect.left=0
-        waveViewbufferRect.right=width
+        waveViewbufferRect.left= MARKER_SIZE
+        waveViewbufferRect.right=width- MARKER_SIZE
         waveViewbufferRect.bottom=height
         if (waveViewBuffer != null) {
             canvas.drawBitmap(waveViewBuffer!!, null, waveViewbufferRect, null)
         }
-        startMarkerX =((width - 2*MARKER_SIZE)*startMarkerPosition + MARKER_SIZE).toInt()
+        startMarkerX =((width - 2*MARKER_SIZE)*startMarkerPosition).toInt()
         startMarkerY = height - MARKER_SIZE
-        canvas.drawCircle(startMarkerX.toFloat(),startMarkerY.toFloat(),
-            MARKER_SIZE.toFloat(),markersColor)
-        endMarkerX=((width - 2*MARKER_SIZE)*endMarkerPosition + MARKER_SIZE).toInt()
+        endMarkerX=((width - 2*MARKER_SIZE)*endMarkerPosition).toInt()
         endMarkerY = height - MARKER_SIZE
-        canvas.drawCircle(endMarkerX.toFloat(),endMarkerY.toFloat(),
-            MARKER_SIZE.toFloat(),markersColor)
+        canvas.drawRect(startMarkerX+ MARKER_SIZE.toFloat(),
+            startMarkerY.toFloat()- MARKER_LINE_WIDTH.toFloat()/2,
+            endMarkerX + MARKER_SIZE.toFloat(),
+            endMarkerY.toFloat()+ MARKER_LINE_WIDTH.toFloat()/2,
+            sampleMarkersColor
+            )
+        canvas.drawCircle(startMarkerX.toFloat()+ MARKER_SIZE.toFloat(),startMarkerY.toFloat(),
+            MARKER_SIZE.toFloat(),sampleMarkersColor)
+
+        canvas.drawCircle(endMarkerX.toFloat()+ MARKER_SIZE.toFloat(),endMarkerY.toFloat(),
+            MARKER_SIZE.toFloat(),sampleMarkersColor)
+        canvas.drawRoundRect(MARKER_SIZE.toFloat()- MARKER_LINE_WIDTH.toFloat()/2 + startMarkerX.toFloat(),
+            0.0f,MARKER_SIZE.toFloat()+ MARKER_LINE_WIDTH.toFloat()/2 + startMarkerX.toFloat(),
+            height.toFloat()-2* MARKER_SIZE.toFloat(),
+            MARKER_LINE_WIDTH.toFloat()/2.0f,MARKER_LINE_WIDTH.toFloat()/2.0f,sampleMarkersColor)
+        canvas.drawRoundRect(MARKER_SIZE.toFloat()- MARKER_LINE_WIDTH.toFloat()/2 + endMarkerX.toFloat(),
+            0.0f,MARKER_SIZE.toFloat()+ MARKER_LINE_WIDTH.toFloat()/2 + endMarkerX.toFloat(),
+            height.toFloat()-2* MARKER_SIZE.toFloat(),
+            MARKER_LINE_WIDTH.toFloat()/2.0f,MARKER_LINE_WIDTH.toFloat()/2.0f,sampleMarkersColor)
+
 
         loopMarkerStart = ((width-2* MARKER_SIZE)*loopStartMarkerPosition).toInt()
-        canvas.drawRoundRect(MARKER_SIZE.toFloat()- MARKER_LINE_WIDTH.toFloat()/2 + loopMarkerStart.toFloat(),
-            0.0f,MARKER_SIZE.toFloat()+ MARKER_LINE_WIDTH.toFloat()/2 + loopMarkerStart.toFloat(),
-            height.toFloat()-2* MARKER_SIZE.toFloat(),
-            MARKER_LINE_WIDTH.toFloat()/2.0f,MARKER_LINE_WIDTH.toFloat()/2.0f,markersColor)
         loopMarkerEnd = ((width-2* MARKER_SIZE)*loopEndMarkerPosition).toInt()
+        canvas.drawCircle(loopMarkerStart.toFloat()+ MARKER_SIZE.toFloat(), MARKER_SIZE.toFloat(),
+            MARKER_SIZE.toFloat(),loopMarkersColor)
+        canvas.drawCircle(loopMarkerEnd.toFloat() + MARKER_SIZE.toFloat(), MARKER_SIZE.toFloat(),
+            MARKER_SIZE.toFloat(),loopMarkersColor)
+        canvas.drawRoundRect(MARKER_SIZE.toFloat()+ MARKER_LINE_WIDTH.toFloat()/2 + loopMarkerStart.toFloat(),
+            height - 2* LOOP_SIGN_RADIUS.toFloat() - 2* MARKER_SIZE.toFloat() - LOOP_SIGN_MARGIN.toFloat(),
+            MARKER_SIZE.toFloat()- MARKER_LINE_WIDTH.toFloat()/2 + loopMarkerEnd.toFloat(),
+            height  - 2* MARKER_SIZE.toFloat() - LOOP_SIGN_MARGIN.toFloat(),
+            LOOP_SIGN_RADIUS.toFloat(),
+            LOOP_SIGN_RADIUS.toFloat(),
+            loopSignColor
+            )
+        canvas.drawRoundRect(MARKER_SIZE.toFloat()- MARKER_LINE_WIDTH.toFloat()/2 + loopMarkerStart.toFloat(),
+            2* MARKER_SIZE.toFloat(),MARKER_SIZE.toFloat()+ MARKER_LINE_WIDTH.toFloat()/2 + loopMarkerStart.toFloat(),
+            height.toFloat(),
+            MARKER_LINE_WIDTH.toFloat()/2.0f,MARKER_LINE_WIDTH.toFloat()/2.0f,loopMarkersColor)
         canvas.drawRoundRect(MARKER_SIZE.toFloat()- MARKER_LINE_WIDTH.toFloat()/2 + loopMarkerEnd.toFloat(),
-            0.0f,MARKER_SIZE.toFloat()+ MARKER_LINE_WIDTH.toFloat()/2 + loopMarkerEnd.toFloat(),
-            height.toFloat()-2* MARKER_SIZE.toFloat(),
-            MARKER_LINE_WIDTH.toFloat()/2.0f,MARKER_LINE_WIDTH.toFloat()/2.0f,markersColor)
+            2* MARKER_SIZE.toFloat(),MARKER_SIZE.toFloat()+ MARKER_LINE_WIDTH.toFloat()/2 + loopMarkerEnd.toFloat(),
+            height.toFloat(),
+            MARKER_LINE_WIDTH.toFloat()/2.0f,MARKER_LINE_WIDTH.toFloat()/2.0f,loopMarkersColor)
+
 
     }
 
@@ -89,19 +131,19 @@ class WaveDisplay(context: Context, attributes: AttributeSet): View(context,attr
         when (event?.action)
         {
             MotionEvent.ACTION_DOWN -> {
-                if ((event.x - startMarkerX)*(event.x - startMarkerX) + (event.y - startMarkerY)*(event.y - startMarkerY) < MARKER_SIZE* MARKER_SIZE)
+                if ((event.x - (startMarkerX+ MARKER_SIZE))*(event.x - (startMarkerX+ MARKER_SIZE)) + (event.y - startMarkerY)*(event.y - startMarkerY) < MARKER_SIZE* MARKER_SIZE)
                 {
                     dragStart=WaveDisplayDragStart(event.x.toInt(),WaveDisplayDragAnchorType.SAMPLE_START)
                 }
-                else if ((event.x - endMarkerX)*(event.x - endMarkerX) + (event.y - endMarkerY)*(event.y - endMarkerY) < MARKER_SIZE* MARKER_SIZE)
+                else if ((event.x - (endMarkerX+ MARKER_SIZE))*(event.x - (endMarkerX+ MARKER_SIZE)) + (event.y - endMarkerY)*(event.y - endMarkerY) < MARKER_SIZE* MARKER_SIZE)
                 {
                     dragStart=WaveDisplayDragStart(event.x.toInt(),WaveDisplayDragAnchorType.SAMPLE_END)
                 }
-                else if ((event.y < height - 2* MARKER_SIZE) && abs(event.x - loopMarkerStart)< MARKER_SIZE)
+                else if ((event.x - (loopMarkerStart+ MARKER_SIZE))*(event.x - (loopMarkerStart+ MARKER_SIZE)) + (event.y - MARKER_SIZE)*(event.y - MARKER_SIZE) < MARKER_SIZE* MARKER_SIZE)
                 {
                     dragStart= WaveDisplayDragStart(event.x.toInt(),WaveDisplayDragAnchorType.LOOP_START)
                 }
-                else if ((event.y < height - 2* MARKER_SIZE) && abs(event.x - loopMarkerEnd)< MARKER_SIZE)
+                else if ((event.x - (loopMarkerEnd+ MARKER_SIZE))*(event.x - (loopMarkerEnd+ MARKER_SIZE)) + (event.y - MARKER_SIZE)*(event.y - MARKER_SIZE) < MARKER_SIZE* MARKER_SIZE)
                 {
                     dragStart = WaveDisplayDragStart(event.x.toInt(),WaveDisplayDragAnchorType.LOOP_END)
                 }
@@ -164,7 +206,13 @@ class WaveDisplay(context: Context, attributes: AttributeSet): View(context,attr
 
     private fun validateMarkers(sampleStart:Float,sampleEnd:Float,loopStart:Float,loopEnd:Float):Boolean
     {
-        return (sampleStart < sampleEnd) && (sampleStart <= loopEnd) && (loopStart < loopEnd) && (loopEnd <= sampleEnd)
+        return (sampleStart < sampleEnd)
+                && (sampleStart <= loopStart)
+                && (loopStart < loopEnd)
+                && (loopEnd <= sampleEnd)
+                && sampleStart >= 0.0f
+                && sampleStart <= 1.0f
+                && sampleEnd <= 1.0f
     }
 
     private fun toRelativeWidth(pos: Int): Float
