0caaf3ec29969d73e884828968bdbc18be68659e
Stonerose35 <fuerholz@gmx.ch>
2024-02-19 22:22:38 +0100

    bugfix: converting color to int

--- app/src/main/java/ch/sr35/touchsamplesynth/graphics/RgbColor.kt
+++ app/src/main/java/ch/sr35/touchsamplesynth/graphics/RgbColor.kt
@@ -8,7 +8,7 @@ class RgbColor(var r: Int,var g: Int,var b: Int): Serializable,Cloneable {
 
     fun toColorInt(): Int
     {
-        return 0xff shl 24 or (r and 0xff shl 16) or (g and 0xff shl 8) or (g and 0xff)
+        return 0xff shl 24 or (r and 0xff shl 16) or (g and 0xff shl 8) or (b and 0xff)
     }
 
     public override fun clone(): RgbColor {
