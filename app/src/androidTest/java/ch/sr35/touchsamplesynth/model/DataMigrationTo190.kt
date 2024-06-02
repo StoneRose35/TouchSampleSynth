7ed27d5448fbf4fa96955a0c775dc502a1a71c29
Stonerose35 <fuerholz@gmx.ch>
2024-06-02 22:31:40 +0200

    corrected presets and added a more meaningful and specific test from the migration from 1.8.7 to 1.9.0

--- app/src/androidTest/java/ch/sr35/touchsamplesynth/model/DataMigrationTo190.kt
+++ app/src/androidTest/java/ch/sr35/touchsamplesynth/model/DataMigrationTo190.kt
@@ -2,7 +2,6 @@ package ch.sr35.touchsamplesynth.model
 
 import androidx.test.platform.app.InstrumentationRegistry
 import com.google.gson.Gson
-import com.google.gson.JsonElement
 import org.junit.Assert
 import org.junit.Test
 
@@ -11,10 +10,12 @@ class DataMigrationTo190 {
     @Test
     fun migrate()
     {
-        val currentJson = InstrumentationRegistry.getInstrumentation().targetContext.assets.open("defaultPresets.json").reader().readText()
+        val currentJson = InstrumentationRegistry.getInstrumentation().context.assets.open("defaultPresets1_8_6.json").reader().readText()
         val newJson = DataUpdater.updatersList.find { it -> it.versionTo == Version(1,9,0) && it.versionFrom == Version(1,8,6) }
             ?.processData(currentJson)
-
-        Assert.assertTrue(Gson().fromJson(newJson,JsonElement::class.java).isJsonObject)
+        val newScenes = Gson().fromJson(newJson,SceneListP::class.java)
+        Assert.assertTrue(newScenes.screenResolutionX >= 0)
+        Assert.assertTrue(newScenes.screenResolutionY >= 0)
+        Assert.assertTrue(newScenes.scenes.size==9)
     }
 }
