45c126d71df9f85b5bb254b79ec7e089f75b7edc
Stonerose35 <fuerholz@gmx.ch>
2024-09-28 23:25:32 +0200

    bugfix: set installflag to true in any case after installation

--- app/src/main/java/ch/sr35/touchsamplesynth/DefaultScenesInstaller.kt
+++ app/src/main/java/ch/sr35/touchsamplesynth/DefaultScenesInstaller.kt
@@ -2,7 +2,6 @@ package ch.sr35.touchsamplesynth
 
 import android.os.Handler
 import android.os.Looper
-import android.text.method.Touch
 import ch.sr35.touchsamplesynth.model.PersistableInstrument
 import ch.sr35.touchsamplesynth.model.PersistableInstrumentDeserializer
 import ch.sr35.touchsamplesynth.model.SamplerP
